0b0c312ea5fbb34a55306c877138fc5ad2eed177
ire-design <irenemwikalii04@gmail.com>
2024-07-20 11:05:05 +0300

    songData

--- site.js
+++ site.js
@@ -16,6 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
         showData(data);
     }
 
+    //display the song details
+    function showData(data){
+        result.innerHTML = `
+            <ul class="songs">
+                ${data.data.map(song => `  
+                    <li>
+                        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+                    </li>
+                `).join('')}
+            </ul>
+        `;
+
+        more.innerHTML = ''
+    }
 
     
 
