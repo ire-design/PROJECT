a9fc02903b592594a9e902bee31de52d1127a7d5
ire-design <irenemwikalii04@gmail.com>
2024-07-20 10:51:57 +0300

    get HTML

--- site.js
+++ site.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form')
+    const search = document.getElementById('search')
+    const result = document.getElementById('result')
+    const more = document.getElementById('more')
+    const themeToggle = document.getElementById('theme-toggle')
 
 });
